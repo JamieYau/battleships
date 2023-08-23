33a17264519e886cc9dace8a30f699d6a17bc272
JamieYau <jamie.yau2002@me.com>
2023-08-23 20:08:36 +0100

    Create Player Class

---
+++ __tests__/Player.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from "vitest";
+
+describe("Player", () => {
+  describe("constructor", () => {
+    it("logs a message", () => {
+      expect(true).toBe(true);
+    });
+  });
+});
