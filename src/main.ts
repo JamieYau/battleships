2b71b2edba1a22c18c286d8b9fac4e8272c75d75
JamieYau <jamie.yau2002@me.com>
2023-08-27 21:52:03 +0100

    Add toggle for dragging class to corresponding ship

--- src/main.ts
+++ src/main.ts
@@ -23,36 +23,51 @@ form.addEventListener("submit", (event) => {
   // Show the ship placement screen UI
   showShipPlacementScreen(playerName);
 
-  // Add event listener to the placement grid cells
-  function handleCellClick(event: Event) {
-    const cell = event.target as HTMLElement;
-    const row = cell.dataset.row;
-    const col = cell.dataset.col;
-    if (!row || !col) return;
-    // use the game module's ship list
-    console.log(`row: ${row}, col: ${col}`);
-  }
-
   const gridCells = document.querySelectorAll(".grid-cell");
   gridCells.forEach((cell) => {
-    cell.addEventListener("click", handleCellClick);
+    cell.addEventListener("dragover", (event) => {
+      event.preventDefault();
+    });
+
+    cell.addEventListener("drop", (event: Event) => {
+      event.preventDefault();
+      const targetCell = event.target as HTMLElement; // Cast to HTMLElement
+      const shipLength = (event as DragEvent).dataTransfer?.getData(
+        "text/plain"
+      );
+      if (!shipLength) return;
+
+      // Handle ship placement here
+      const row = parseInt(targetCell.dataset.row || "");
+      const col = parseInt(targetCell.dataset.col || "");
+      // Update ship positions on the grid
+      console.log(`row: ${row}, col: ${col}`);
+      console.log(`shipLength: ${shipLength}`);
+    });
   });
 
   // Attach event listeners for ship segments
-  const shipSegments = document.querySelectorAll(".ship-segment") as NodeListOf <HTMLDivElement>;
+  const shipSegments = document.querySelectorAll(
+    ".ship-segment"
+  ) as NodeListOf<HTMLDivElement>;
   shipSegments.forEach((segment) => {
     segment.addEventListener("dragstart", (event) => {
       if (!(event.target instanceof HTMLElement)) return;
       event.target.classList.add("dragging");
-      const shipLength = event.target.parentElement?.dataset.shipLength;
+      const ship = event.target.parentElement;
+      ship?.classList.add("dragging");
+      const shipLength = ship?.dataset.shipLength;
       if (!shipLength) return;
       console.log(shipLength);
-      event.dataTransfer?.setData("text/plain", shipLength);
+      const dragEvent = event as DragEvent; // Cast to DragEvent
+      dragEvent.dataTransfer?.setData("text/plain", shipLength);
     });
 
     segment.addEventListener("dragend", (event) => {
       if (!(event.target instanceof HTMLElement)) return;
       event.target.classList.remove("dragging");
+      const ship = event.target.parentElement;
+      ship?.classList.remove("dragging");
     });
   });
 });
