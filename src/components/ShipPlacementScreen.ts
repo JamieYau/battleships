ea5beaf78d712fed6bd984935e855bfeb9f4b4a3
JamieYau <jamie.yau2002@me.com>
2023-08-27 21:03:48 +0100

    Add styling to ships

--- src/components/ShipPlacementScreen.ts
+++ src/components/ShipPlacementScreen.ts
@@ -1,4 +1,5 @@
 import { Game } from "../modules/Game";
+import Ship from "../modules/Ship";
 export function showShipPlacementScreen(playerName: string): void {
   const container = document.getElementById("app");
   if (!container) return;
@@ -9,10 +10,11 @@ export function showShipPlacementScreen(playerName: string): void {
         <div id="placement-grid" class="grid-container">
             ${generateGridCells()}
         </div>
-        <div id="ship-list">${generateShipList()}</div>
+        <div id="ship-list"></div>
     </div>
   `;
   container.innerHTML = template;
+  generateShipList();
 }
 
 function generateGridCells(): string {
@@ -25,16 +27,26 @@ function generateGridCells(): string {
   return gridCells;
 }
 
-function generateShipList(): string {
+function generateShipList(): void {
   const shipList = Game.shipList;
-  let shipListHTML = "";
+  const shipListContainer = document.getElementById("ship-list");
+  if (!shipListContainer) return;
   shipList.forEach((ship) => {
-    shipListHTML += `
-            <div class="ship-list-item">
-                <span>${ship}</span>
-                <span>${ship.length}</span>
-            </div>
-        `;
+    const shipItem = generateShip(ship);
+    shipListContainer.appendChild(shipItem);
   });
-  return shipListHTML;
+}
+
+function generateShip(ship: Ship): HTMLDivElement {
+  // create a div ship-item with ship.length number of divs
+
+  const shipItem = document.createElement("div");
+  shipItem.classList.add("ship-item");
+  shipItem.dataset.shipLength = ship.length.toString();
+  for (let i = 0; i < ship.length; i++) {
+    const shipSegment = document.createElement("div");
+    shipSegment.classList.add("ship-segment");
+    shipItem.appendChild(shipSegment);
+  }
+  return shipItem;
 }
